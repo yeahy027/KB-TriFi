778b59999714af55c778bafe5719229f0473f83f
chojayoung <26006@naver.com>
2025-04-07 17:07:22 +0900

    Add : router 애 login, register 추가

--- trifi_project/src/router/index.js
+++ trifi_project/src/router/index.js
@@ -1,13 +1,26 @@
+import { createRouter, createWebHistory } from 'vue-router'
 import AppLayout from '../components/AppLayout.vue'
+import Login from './views/Login.vue'
+import Register from './views/Register.vue'
 
 const router = createRouter({
   history: createWebHistory('/'),
   routes: [
     {
       path: '/',
+      name: 'login',
+      component: Login,
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register,
+    },
+    {
+      path: '/home',
       name: 'home',
       component: AppLayout,
-    },
+    }
   ],
 
 })
