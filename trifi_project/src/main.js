3f2a5b6042661f8a6bda286af5717d0768c3ea19
chojayoung <26006@naver.com>
2025-04-08 17:03:09 +0900

    Add : main.js 코드에 차트 리사이즈 코드 추가

--- trifi_project/src/main.js
+++ trifi_project/src/main.js
@@ -1,8 +1,9 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { resizeAllCharts } from './utils/chartManager'
 
 import App from './App.vue'
 import router from './router'
@@ -11,5 +12,10 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+// 🔥 윈도우 리사이즈 시 전체 차트 리사이즈
+window.addEventListener('resize', () => {
+  resizeAllCharts()
+})
 
 app.mount('#app')
