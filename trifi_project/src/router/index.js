eb05c51eb768cf1dc2d53ada57db78dc0641b95d
chojayoung <26006@naver.com>
2025-04-08 16:28:47 +0900

    Add : 라우터 index.js 페이지에 페이지별 이름 추가

--- trifi_project/src/router/index.js
+++ trifi_project/src/router/index.js
@@ -8,23 +8,33 @@ const router = createRouter({
       path: '/',
       name: 'login',
       component: Login,
+      meta: { title: '로그인' }
     },
     {
       path: '/register',
       name: 'register',
       component: () => import('../pages/RegisterPage.vue'),
+      meta: { title: '회원가입' }
     },
     {
       path: '/stat',
       name: 'stat',
       component: () => import('../pages/StatsPage.vue'),
+      meta: { title: '통계 분석 페이지' }
     },
     {
       path: '/home',
       name: 'home',
       component: () => import('../pages/CalendarPage.vue'),
+      meta: { title: '캘린더' }
     },
   ],
 
 })
+
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title || '기본 제목'
+  next()
+})
+
 export default router
