aec7d80745b0dd8fb5577d880b69a4f677eeae22
chojayoung <26006@naver.com>
2025-04-08 10:18:20 +0900

    Add : vite.config.js 에 프록시 추가

--- trifi_project/vite.config.js
+++ trifi_project/vite.config.js
@@ -13,4 +13,13 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000', // 실제 API 서버 주소
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
