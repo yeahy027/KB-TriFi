454b1cbfb54c2e4773e11549a934b443ec587255
capstonn <suhyun6655@gmail.com>
2025-04-10 15:26:14 +0900

    오류 수정

--- trifi_project/src/components/AppSidebar.vue
+++ trifi_project/src/components/AppSidebar.vue
@@ -11,10 +11,6 @@
         <span style="color: #e44027">Keeper</span>
       </h5>
     </div>
-<<<<<<< HEAD
-    
-=======
->>>>>>> 7d841813b6fc44f1de2650afeda14dfa0f0195c0
     <ul class="nav flex-column gap-2">
       <li v-for="item in menuItems" :key="item.to">
         <router-link
